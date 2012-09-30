6f357f80b9c12350a74a1c0f98db5ada4911cdf3
Tomas Barton <barton.tomas@gmail.com>
2012-09-30 17:18:45 +0200

    added test for modification of passed array

--- 02/exerices/spec/iterator_spec.rb
+++ 02/exerices/spec/iterator_spec.rb
@@ -46,5 +46,15 @@ describe "Odd iterator" do
     res.should have(0).items
     res.should be_empty
   end
-
+  
+  it "should not modify original array" do 
+    ary = [2,2,2,5,5,8]
+    ary2 = ary.dup
+    res = odd_elements(ary)
+    res[0].should == 2
+    res[1].should == 5
+    res[2].should == 8
+    ary.should == ary2
+  end
+  
 end
